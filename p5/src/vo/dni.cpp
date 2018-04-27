887a6e8b144e254cdb9bd43fd90f65e7324f8383
charly_poket <carlosyroco@hotmail.com>
2018-04-27 18:23:25 +0000

    errata

--- p5/src/vo/dni.cpp
+++ p5/src/vo/dni.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Author      	: David Dorta Acosta
-// email       	: alu0100851236@ull.edu.es
-// Asignatura  	: Algoritmos y Estructura de Datos Avanzadas
-// Curso			: 2017-2018
-// Práctica 5	: Implementación de métodos de ordenación
-//============================================================================
-
 #include "dni.hpp"
 #include <ctime>
 
